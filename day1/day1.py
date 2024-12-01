2d2b17c5503c5df45784b5f572a149803f903ebf
tildejustin <spacepepper3.14@gmail.com>
2024-12-01 01:36:14 -0500

    day 1 (it's 1 am and i forgot how to code)

---
+++ day1/day1.py
@@ -0,0 +1,28 @@
+with open("input") as f:
+    a = []
+    b = []
+    for l in f.readlines():
+        c = l.split()
+        a.append(int(c[0]))
+        b.append(int(c[1]))
+
+a.sort()
+b.sort()
+sum = 0
+for d in zip(a, b):
+    sum += abs(d[0] - d[1])
+print(sum)
+
+# part 2
+with open("input") as f:
+    a = set()
+    b = []
+    for l in f.readlines():
+        c = l.split()
+        a.add(int(c[0]))
+        b.append(int(c[1]))
+
+sum = 0
+for d in a:
+    sum += d * b.count(d)
+print(sum)
