8ad28f8fb45919c369623b0085490a30f111881f
tildejustin <spacepepper3.14@gmail.com>
2024-12-08 18:23:14 -0500

    partial functional impl

--- day7/day7.py
+++ day7/day7.py
@@ -17,13 +17,13 @@ def apply_2(terms, solution) -> bool:
     # reduce to a reduce
     for i in range(perms):
         perm = bin(i)[2:].zfill(length)
-        result = terms[0]
+        result = int(terms[0])
         for i, term in enumerate(terms[1:]):
             match perm[i]:
                 case "0":
-                    result += term
+                    result += int(term)
                 case "1":
-                    result *= term
+                    result *= int(term)
         if result == solution:
             return True
     return False
@@ -38,33 +38,29 @@ def apply_3(terms, solution) -> bool:
             perm = to_ternary(i)
             ternary_cache[i] = perm
         perm = perm.zfill(length)
-        result = terms[0]
+        result = int(terms[0])
         for i, term in enumerate(terms[1:]):
             match perm[i]:
                 case "0":
-                    result += term
+                    result += int(term)
                 case "1":
-                    result *= term
+                    result *= int(term)
                 case "2":
-                    result = int(str(result) + str(term))
+                    result = int(str(result) + term)
         if result == solution:
             return True
     return False
 
 
 with open("input") as f:
-    result = 0
     result2 = 0
+    print(sum(map(lambda x: x[0], filter(lambda x: apply_2(x[1], x[0]), [(int(line.split(":")[0]), line.split(":")[1].strip().split(" ")) for line in f.readlines()]))))
+    f.seek(0)
     for line in f.readlines():
         solution = int(line.split(":")[0])
-        terms = list(map(int, line.split(":")[1].strip().split(" ")))
+        terms = line.split(":")[1].strip().split(" ")
         # make every combination of 0|1 for list of len n
         # which is also just the digits of a binary number of len n?
-        # sum(filter(lambda x: apply(x, result), terms))
-        if apply_2(terms, solution):
-            result += solution
         if apply_3(terms, solution):
-            print(solution)
             result2 += solution
-    print(result)
     print(result2)
